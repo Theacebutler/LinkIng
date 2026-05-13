a89f70e703066fabc3729725fb90d13bd36e735b
theacebutler <theacebutler@gmail.com>
2026-05-13 11:03:42 -0400

    ADD env frontend url

--- api/handlers/api_resources.ts
+++ api/handlers/api_resources.ts
@@ -3,16 +3,18 @@ import { resourcesTable } from "../db/schema";
 import type { Resource } from "../shered/types";
 import { handleScreenshot } from "../utils/handleScreenshot";
 
+const FRUNTEND_URL = process.env.FRUNTEND_URL as string;
+
 export async function apiResourcesGet(): Promise<Response> {
   const resources = await db.select().from(resourcesTable);
   const res = Response.json(resources);
-  res.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.headers.set("Access-Control-Allow-Origin", FRUNTEND_URL);
   return res;
 }
 
 export async function apiResourcesOpts(): Promise<Response> {
   const res = Response.json({});
-  res.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.headers.set("Access-Control-Allow-Origin", FRUNTEND_URL);
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin");
   return res;
@@ -32,6 +34,6 @@ export async function apiResourcesPost(req: Bun.BunRequest<"/api/resources">, se
 
   handleScreenshot(server, newResource.resourceUrl, id?.insertId)
   const res = Response.json(newResource);
-  res.headers.set("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.headers.set("Access-Control-Allow-Origin", FRUNTEND_URL);
   return res;
 }
