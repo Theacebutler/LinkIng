3898e6b8f29b34d4199493961412f009e60ac00f
theacebutler <theacebutler@gmail.com>
2026-05-13 13:44:46 -0400

    ADD migrations and db conn from .env

--- api/db/index.ts
+++ api/db/index.ts
@@ -1,5 +1,8 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
+import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-const sqlite = new Database(process.env.DB_FILE_NAME as string || "db.sqlite", { create: true })
+const sqlite = new Database(process.env.BD_CONN as string);
 export const db = drizzle({ client: sqlite })
+
+migrate(db, { migrationsFolder: "./drizzle" })
