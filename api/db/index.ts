35b1deb664fccd00b87afccff23d191017456141
theacebutler <theacebutler@gmail.com>
2026-05-13 23:09:07 -0400

    FIX typos

--- api/db/index.ts
+++ api/db/index.ts
@@ -2,7 +2,7 @@ import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
 
-const sqlite = new Database(process.env.BD_CONN as string);
+const sqlite = new Database(process.env.DB_CONN as string);
 export const db = drizzle({ client: sqlite })
 
 migrate(db, { migrationsFolder: "./drizzle" })
--- api/handlers/api_resources.ts
+++ api/handlers/api_resources.ts
@@ -4,7 +4,7 @@ import { resourcesTable } from "../db/schema";
 import type { Resource } from "../shered/types";
 import { handleScreenshot } from "../utils/handleScreenshot";
 
-const FRUNTEND_URL = process.env.FRUNTEND_URL as string;
+const FRONTEND_URL = process.env.FRONTEND_URL as string;
 
 export async function apiResourcesGet(name: string): Promise<Response> {
   const resources = await db.select()
@@ -12,13 +12,13 @@ export async function apiResourcesGet(name: string): Promise<Response> {
     .where((resouce) => eq(resouce.owner, name))
     .orderBy(desc(resourcesTable.createdAt));
   const res = Response.json(resources);
-  res.headers.set("Access-Control-Allow-Origin", FRUNTEND_URL);
+  res.headers.set("Access-Control-Allow-Origin", FRONTEND_URL);
   return res;
 }
 
 export async function apiResourcesOpts(): Promise<Response> {
   const res = Response.json({});
-  res.headers.set("Access-Control-Allow-Origin", FRUNTEND_URL);
+  res.headers.set("Access-Control-Allow-Origin", FRONTEND_URL);
   res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
   res.headers.set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Origin");
   return res;
@@ -38,6 +38,6 @@ export async function apiResourcesPost(req: Bun.BunRequest, server: Bun.Server<u
 
   handleScreenshot(server, newResource.resourceUrl, id?.insertId)
   const res = Response.json(newResource);
-  res.headers.set("Access-Control-Allow-Origin", FRUNTEND_URL);
+  res.headers.set("Access-Control-Allow-Origin", FRONTEND_URL);
   return res;
 }
