84330a050ee956aa8e3dc79fdbfa8cee3f4a35ae
theacebutler <theacebutler@gmail.com>
2026-05-17 23:39:40 -0400

    FIX type name to uppercase

--- api/shered/types.d.ts
+++ api/shered/types.d.ts
@@ -3,5 +3,5 @@ import { type InferSelectModel, type InferInsertModel } from "drizzle-orm";
 
 
 type Resource = InferInsertModel<typeof resourcesTable>;
-type user = InferInsertModel<typeof usersTable>;
+type User = InferInsertModel<typeof usersTable>;
 type Screenshot = InferInsertModel<typeof screenshotsTable>;
