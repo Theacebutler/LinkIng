72fc6dc8560a9b41732d73851709fa47aa2e8cb1
theacebutler <theacebutler@gmail.com>
2026-05-18 14:48:21 -0400

    ADD login handler

---
+++ frontend/src/utils/login.ts
@@ -0,0 +1,33 @@
+import Cookies from "js-cookie"
+import { config } from "../../config"
+import { setAccessTokenCookie, setRefreshTokenCookie } from "./cookies"
+
+interface LoginResponse {
+  accessToken: string,
+  refreshToken: string,
+  tokenType: string,
+  expiresIn: number
+}
+
+export default async function login(username: string, password: string) {
+  const res = await fetch(`${config.VITE_API_URL}/users/login/`, {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password
+    })
+  })
+  const data = await res.json() as LoginResponse
+  switch (res.status) {
+    case 200:
+      Cookies.set('accessToken', setAccessTokenCookie(data.accessToken))
+      Cookies.set('refreshToken', setRefreshTokenCookie(data.refreshToken))
+      Cookies.set('username', username)
+      break
+    case 400:
+      break
+    case 401:
+      break
+    default:
+  }
+}
