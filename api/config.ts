f4d57cf28c9fa41b9f274f0b9f16e6667ecc5a3d
theacebutler <theacebutler@gmail.com>
2026-05-17 23:21:29 -0400

    FIX time calc in config

--- api/config.ts
+++ api/config.ts
@@ -3,8 +3,8 @@ export const config = {
   REFRESH_SECRET: Bun.env.REFRESH_SECRET,
   FRONTEND_URL: Bun.env.FRONTEND_URL,
   DB_CONN: Bun.env.DB_CONN,
-  TOKEN_EXP: 60 * 60 * 1, // expires in 1 hour
-  REFRESH_EXP: 60 * 60 * 12, // expires in 1 hour
+  TOKEN_EXP: "3h", // expires is 3 hours
+  REFRESH_EXP: "12h", // expires is 12 hours
   HASH_ALG: 'HS256',
   JWT_ISSUER: "gatherlink",
 }
