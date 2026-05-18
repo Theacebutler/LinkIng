ca404debaa0770866ab74cd281022398b92919e3
theacebutler <theacebutler@gmail.com>
2026-05-18 14:45:35 -0400

    ADD access control heaer to response util

--- api/utils/jsonResponeUtil.ts
+++ api/utils/jsonResponeUtil.ts
@@ -1,8 +1,13 @@
+import { config } from "../config";
+
 // Helper for JSON responses
 export function json(data: object, status: number = 200): Response {
   return new Response(JSON.stringify(data), {
     status,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": config.FRONTEND_URL as string,
+    },
   });
 }
 export const SALT_ROUNDS = 10
