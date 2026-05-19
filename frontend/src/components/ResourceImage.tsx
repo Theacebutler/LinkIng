6d882c210b8b134fa82eb4fd369fa8608192e9d6
theacebutler <theacebutler@gmail.com>
2026-05-18 22:38:25 -0400

    FIX dont load the image until the url is valid
    this is bc the post request to create a new resouce will not return
    until the image url is created

--- frontend/src/components/ResourceImage.tsx
+++ frontend/src/components/ResourceImage.tsx
@@ -1,51 +1,46 @@
-import { useState, useEffect, useRef } from "react";
 import type { Resource } from "../types/resource";
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
 export default function ResoueceImage({ resource }: { resource: Resource }) {
-  const [imageUrl, setImageUrl] = useState('');
-  const [loaded, setLoaded] = useState(false);
-  const pollingRef = useRef<number | undefined>(undefined);
+  const imageUrl = `${VITE_API_URL}/resources/screenshots/${resource.id}`
+  // const [imageUrl, setImageUrl] = useState('');
+  // const [loaded, setLoaded] = useState(false);
+  // const pollingRef = useRef<number | undefined>(undefined);
 
-  async function pollingImage() {
-    if (!resource.id) return
-    if (loaded) return;
-    const data = await fetch(`${VITE_API_URL}/resources/screenshots/${resource.id}`)
-    switch (data.status) {
-      case 200:
-        setLoaded(true);
-        setImageUrl(`${VITE_API_URL}/resources/screenshots/${resource.id}`);
-        break;
-      default:
-        setImageUrl('');
-        setLoaded(false);
-        setTimeout(() => {
-          pollingImage()
-        }, 300)
-        break;
-    };
-  };
+  // async function pollingImage() {
+  //   if (!resource.id) return
+  //   if (loaded) return;
+  //   const data = await fetch(`${VITE_API_URL}/resources/screenshots/${resource.id}`)
+  //   switch (data.status) {
+  //     case 200:
+  //       setLoaded(true);
+  //       setImageUrl(`${VITE_API_URL}/resources/screenshots/${resource.id}`);
+  //       break;
+  //     default:
+  //       setImageUrl('');
+  //       setLoaded(false);
+  //       setTimeout(() => {
+  //         pollingImage()
+  //       }, 300)
+  //       break;
+  //   };
+  // };
 
-  useEffect(() => {
-    setTimeout(() => {
-      pollingImage()
-    }, 300)
-  }, [resource]);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     pollingImage()
+  //   }, 300)
+  // }, []);
 
 
-  if (loaded) {
-    return (
-      <div className="mt-3">
-        <img
-          src={imageUrl}
-          alt="Source preview"
-          className="w-full border border-slate-600 rounded bg-white"
-          onLoad={() => setLoaded(true)}
-        />
-      </div>
-    );
-  }
-
-  return null;
+  return (
+    <div className="mt-3">
+      <img
+        src={imageUrl}
+        alt="Source preview"
+        className="w-full border border-slate-600 rounded bg-white"
+      />
+    </div>
+  );
 }
