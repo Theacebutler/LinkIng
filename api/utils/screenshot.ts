59b824ee393b5582c29e1c657d0e90d6769f80de
theacebutler <theacebutler@gmail.com>
2026-05-19 12:37:43 -0400

    ADD pool for better performance

--- api/utils/screenshot.ts
+++ api/utils/screenshot.ts
@@ -1,26 +1,83 @@
-import puppeteer from "puppeteer";
+import * as genericPool from "generic-pool"
+import puppeteer, { type Browser } from "puppeteer";
 import { db } from "../db";
 import { screenshotsTable } from "../db/schema";
 
 
+let screenshot_count = 0
+const MAX_SCREENSHOT_COUNT = 100
+
+const factory = {
+  create: async () => {
+    return await puppeteer.launch({
+      headless: true,
+      args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage",
+        "--disable-gpu"
+      ]
+    });
+  },
+  destroy: async (browser: Browser) => {
+    await browser.close()
+  },
+  validate: async (browser: Browser) => {
+    try {
+      browser.pages()
+      return true
+    } catch {
+      return false
+    }
+  }
+}
+
+const pool = genericPool.createPool(factory, {
+  min: 2,
+  max: 5,
+  testOnBorrow: false,
+  evictionRunIntervalMillis: 60000
+
+})
+
 export async function screenshot(url: string | null | undefined, resourceId: number | undefined): Promise<void> {
   if (!url || !resourceId) return
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox', '--disable-setuid-sandbox']
-  });
+  const browser = await pool.acquire();
+  try {
+    screenshot_count++
+    if (screenshot_count >= MAX_SCREENSHOT_COUNT) {
+      await pool.destroy(browser);
+      screenshot_count = 0
+      return await screenshot(url, resourceId)
+    }
+  } finally {
+    await pool.release(browser);
+  }
   const page = await browser.newPage();
 
   await page.setViewport({ width: 1820, height: 720 });
-  await page.goto(url, { waitUntil: 'networkidle2' });
-
+  try {
+    const res = await page.goto(url, { waitUntil: 'networkidle2' });
+    console.log("url", url, "status", res?.status());
+    if (res?.status() !== 200) {
+      await db.insert(screenshotsTable).values({
+        resourceId: resourceId,
+        image: null
+      })
+      await pool.destroy(browser)
+      return;
+    };
+  } catch (e) {
+    // TODO: ADD LOGGING
+    await pool.destroy(browser)
+  }
 
   const image = await page.screenshot({
     type: 'png',
     encoding: 'base64',
     fullPage: false
   });
-  await browser.close();
+  await pool.destroy(browser)
 
   await db.insert(screenshotsTable).values({
     resourceId: resourceId,
