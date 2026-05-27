57a8eff3b96ec34417eb5e6a4e55ee6b08305c16
Avi B <Theacebutler@gmail.com>
2026-05-27 00:59:15 -0400

    Update inDevAlert.tsx

--- frontend/src/components/inDevAlert.tsx
+++ frontend/src/components/inDevAlert.tsx
@@ -1,7 +1,7 @@
 export default function InDevAlert() {
   return (
     <div className="bg-amber-500/10 border border-red-400 text-red-400 text-center py-2 px-4 text-sm mb-3">
-      This application is in development — you may encounter issues.
+      This application is in development — you may encounter issues such as previews taking some time to load or not loading.
       <br />
       Follow the project on GitHub at <a className="underline" href="https://github.com/theacebutler/linkIng">github.com/theacebutler/linkIng</a>.
     </div>
