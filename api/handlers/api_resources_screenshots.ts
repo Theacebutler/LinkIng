1fd8ccaaabf9df63c9d0cc2d35214624a8dc8972
theacebutler <theacebutler@gmail.com>
2026-05-27 15:18:14 -0400

    ADD try/catch to get image from db

--- api/handlers/api_resources_screenshots.ts
+++ api/handlers/api_resources_screenshots.ts
@@ -13,23 +13,33 @@ export function apiResourceScreenshotOpts() {
 
 export async function apiResourceScreenshotGet(req: Bun.BunRequest<"/api/resources/screenshots/:id">) {
   const id = Number(req.params.id)
-  const image_record = await db.select()
-    .from(screenshotsTable)
-    .where(eq(screenshotsTable.resourceId, id));
-  const image = image_record[0]?.image
-  if (image) {
-    // add the image url to the image in the DB
-    await db.update(resourcesTable)
-      .set({ imageUrl: `/api/resources/screenshots/${id}` })
-      .where(eq(resourcesTable.id, id));
-    const res = new Response(Buffer.from(image, 'base64'), {
-      headers: {
-        'Content-Type': 'image/png',
-        'Access-Control-Allow-Origin': config.FRONTEND_URL,
-      },
-    });
-    return res;
-  } else {
+  try {
+    const image_record = await db.select()
+      .from(screenshotsTable)
+      .where(eq(screenshotsTable.resourceId, id));
+    const image = image_record[0]?.image
+    if (image) {
+      // add the image url to the image in the DB
+      await db.update(resourcesTable)
+        .set({ imageUrl: `/api/resources/screenshots/${id}` })
+        .where(eq(resourcesTable.id, id));
+      const res = new Response(Buffer.from(image, 'base64'), {
+        headers: {
+          'Content-Type': 'image/png',
+          'Access-Control-Allow-Origin': config.FRONTEND_URL,
+        },
+      });
+      return res;
+    } else {
+      return new Response(JSON.stringify({ error: "Image not found" }), {
+        status: 404,
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': config.FRONTEND_URL,
+        },
+      });
+    }
+  } catch (e) {
     return new Response(JSON.stringify({ error: "Image not found" }), {
       status: 404,
       headers: {
