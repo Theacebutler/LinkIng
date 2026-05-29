ef6f2e7e0fdd5ea060d3a77962f3487616796b59
theacebutler <theacebutler@gmail.com>
2026-05-28 21:24:03 -0400

    REMOVE pool and add cloase browser

--- api/utils/screenshot.ts
+++ api/utils/screenshot.ts
@@ -42,17 +42,25 @@ const pool = genericPool.createPool(factory, {
 
 export default async function screenshot(url: string | null | undefined, resourceId: number | undefined): Promise<void> {
   if (!url || !resourceId) return
-  const browser = await pool.acquire();
-  try {
-    screenshot_count++
-    if (screenshot_count >= MAX_SCREENSHOT_COUNT) {
-      await pool.destroy(browser);
-      screenshot_count = 0
-      return await screenshot(url, resourceId)
-    }
-  } finally {
-    await pool.release(browser);
-  }
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: [
+      "--no-sandbox",
+      "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-gpu",
+    ]
+  });
+  // try {
+  //   screenshot_count++
+  //   if (screenshot_count >= MAX_SCREENSHOT_COUNT) {
+  //     await pool.destroy(browser);
+  //     screenshot_count = 0
+  //     return await screenshot(url, resourceId)
+  //   }
+  // } finally {
+  //   await pool.release(browser);
+  // }
 
   const page = await browser.newPage()
     .then((page) => {
