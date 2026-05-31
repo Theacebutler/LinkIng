3d8887428cc48bea60616a2cfad85bfe1d217463
theacebutler <theacebutler@gmail.com>
2026-05-30 23:12:13 -0400

    ADD a job stack for screenshot jobs

--- api/utils/screenshot.ts
+++ api/utils/screenshot.ts
@@ -3,8 +3,42 @@ import { db } from "../db";
 import { screenshotsTable } from "../db/schema";
 import formatMemoryUsage from "./formatMemoryUsage";
 
-export default async function screenshot(url: string | null | undefined, resourceId: number | undefined): Promise<void> {
+type ScreenshotJob = { timeAdded?: number, url: string, resourceId: number, isDone: boolean }
+const screenshotStack = new Array<ScreenshotJob>
+let PROCESSING = false
+
+export default function addToScreenshotStack(url: string | null | undefined, resourceId: number | undefined) {
   if (!url || !resourceId) return
+  const newJob: ScreenshotJob = {
+    url,
+    resourceId,
+    isDone: false,
+    timeAdded: Date.now()
+  }
+  screenshotStack.push(newJob)
+  if (!PROCESSING) handleNextScreenshot()
+}
+
+async function handleNextScreenshot() {
+  if (screenshotStack.length <= 0) return
+  PROCESSING = true
+  const curr = screenshotStack.pop()
+  if (!curr) return
+  try {
+    await screenshot(curr.url, curr.resourceId)
+  } catch (e) {
+    console.error({
+      Error: e,
+      Memory: formatMemoryUsage(),
+      resourceId: curr.resourceId,
+      url: curr.url,
+      age: curr.timeAdded
+    })
+  }
+  PROCESSING = false
+  handleNextScreenshot()
+}
+async function screenshot(url: string, resourceId: number): Promise<void> {
   const browser = await puppeteer.launch({
     headless: true,
     args: [
@@ -35,12 +69,8 @@ export default async function screenshot(url: string | null | undefined, resourc
           image
         })
       }
-    } catch (e) {
-      console.error({
-        Error: e,
-        Memory: formatMemoryUsage(),
-        resourceId: resourceId,
-      })
+    } finally {
+      await page.close()
     }
   } finally {
     await browser.close()
