66d3052e5090cb8fc11d5c7ecceeb204565419bb
theacebutler <theacebutler@gmail.com>
2026-05-31 18:55:18 -0400

    UPDATE tests - all pass

--- api/tests/test_update_route.test.ts
+++ api/tests/test_update_route.test.ts
@@ -5,6 +5,7 @@ import { register } from "../handlers/register";
 import { login } from "../handlers/login";
 import type { Resource } from "../shared/types";
 
+type Response = { message: string; id?: number, error?: string | unknown }
 let token: string;
 let TEST_RESOURCE_ID: number
 const TEST_USER = `test-user-${Math.random().toString(36).substring(7)}`
@@ -44,7 +45,7 @@ describe("TEST: resource CRUD", async () => {
       })
     })
     const res = await addResource(mockRequest)
-    const data = await res.json() as { message: string; id?: number, error?: string | unknown };
+    const data = await res.json() as Response;
     expect(res.status).toBe(200);
     expect(res.ok).toBe(true);
     // test the data.message output
@@ -53,10 +54,27 @@ describe("TEST: resource CRUD", async () => {
     TEST_RESOURCE_ID = data.id as number
   })
 
+  test("TEST get resource", async () => {
+    const mockRequest: Request = new Request(
+      `http://localhost:3000/api/resources/`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      }
+    })
+    const res = await getResources(mockRequest)
+    const data = await res.json() as Resource[];
+    expect(res.ok).toBe(true);
+    const resource = data.find(resource => resource.id === TEST_RESOURCE_ID)
+    expect(resource).toBeDefined()
+    expect(resource?.title).toBe("initial title")
+  })
+
   test("TEST update resource", async () => {
     const mockRequest: Request = new Request(
-      `http://localhost:3000/api/resources/${TEST_RESOURCE_ID}/`, {
-      method: "POST",
+      `http://localhost:3000/api/resources/`, {
+      method: "PATCH",
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
@@ -70,7 +88,7 @@ describe("TEST: resource CRUD", async () => {
       })
     })
     const res = await updateResource(mockRequest)
-    const data = await res.json() as { message: string; id?: number, error?: string | unknown };
+    const data = await res.json() as Response;
 
     expect(res.ok).toBe(true);
     expect(res.status).toBe(200);
@@ -98,16 +116,18 @@ describe("TEST: resource CRUD", async () => {
 
   test("TEST delete resource", async () => {
     const mockRequest: Request = new Request(
-      `http://localhost:3000/api/resources/${TEST_RESOURCE_ID}/`, {
+      `http://localhost:3000/api/resources/`, {
       method: "DELETE",
       headers: {
         "Content-Type": "application/json",
         "Authorization": `Bearer ${token}`
       },
+      body: JSON.stringify({
+        id: TEST_RESOURCE_ID
+      })
     })
     const res = await deleteResource(mockRequest)
-    const data = await res.json() as { message: string; id?: number, error?: string | unknown };
-    console.log("data", data, TEST_RESOURCE_ID)
+    const data = await res.json() as Response;
     expect(res.ok).toBe(true);
     expect(res.status).toBe(200);
     // test the data.message output
