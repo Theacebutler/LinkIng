44c9b60f132f48ef377c3a0afb22aa3d15a77fd3
theacebutler <theacebutler@gmail.com>
2026-05-31 14:33:20 -0400

    ADD some tests

---
+++ api/tests/test_update_route.test.ts
@@ -0,0 +1,103 @@
+import { beforeAll, describe, expect, test } from "bun:test";
+
+import { addResource, getResources, updateResource } from "../handlers/protected";
+import { register } from "../handlers/register";
+import { login } from "../handlers/login";
+import type { Resource } from "../shared/types";
+
+let token: string;
+let TEST_RESOURCE_ID: number
+const TEST_USER = `test-user-${Math.random().toString(36).substring(7)}`
+const TEST_PASSWORD = "test-password"
+
+beforeAll(async () => {
+  const mockRequest: Request = new Request(
+    "http://localhost:3000/api/users/register/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      username: TEST_USER,
+      password: TEST_PASSWORD
+    })
+  })
+  await register(mockRequest)
+  await login(TEST_USER, TEST_PASSWORD, mockRequest.headers)
+    .then(async (res) => {
+      const data = await res.json() as { accessToken: string };
+      token = data.accessToken
+    })
+})
+
+describe("TEST: resource CRUD", async () => {
+  test("TEST add resource", async () => {
+    const mockRequest: Request = new Request(
+      "http://localhost:3000/api/resources/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        title: "initial title",
+        resourceUrl: "https://www.google.com",
+        sourceUrl: "https://www.google.com",
+      })
+    })
+    const res = await addResource(mockRequest)
+    const data = await res.json() as { message: string; id?: number, error?: string | unknown };
+    expect(res.status).toBe(200);
+    expect(res.ok).toBe(true);
+    // test the data.message output
+    expect(data.id).toBeDefined();
+    expect(data.error).toBeUndefined();
+    TEST_RESOURCE_ID = data.id as number
+  })
+
+  test("TEST update resource", async () => {
+    const mockRequest: Request = new Request(
+      `http://localhost:3000/api/resources/${TEST_RESOURCE_ID}/`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        title: "new title",
+        resourceUrl: "https://www.google.com",
+        sourceUrl: "https://www.google.com",
+        owner: TEST_USER,
+        id: TEST_RESOURCE_ID
+      })
+    })
+    const res = await updateResource(mockRequest)
+    const data = await res.json() as { message: string; id?: number, error?: string | unknown };
+
+    expect(res.ok).toBe(true);
+    expect(res.status).toBe(200);
+    // test the data.message output
+    expect(data.error).toBeUndefined();
+  })
+
+  test("CHECK resource update", async () => {
+    const mockRequest: Request = new Request(
+      `http://localhost:3000/api/resources`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+      },
+    })
+    const res = await getResources(mockRequest)
+    const data = await res.json() as Resource[];
+    expect(res.ok).toBe(true);
+    console.log(data);
+
+    // const updatedResource = data.resources.find(resource => resource.id === TEST_RESOURCE_ID)
+    // expect(updatedResource).toBeDefined()
+    // expect(updatedResource?.title).toBe("new title")
+
+  })
+
+
+
+})
