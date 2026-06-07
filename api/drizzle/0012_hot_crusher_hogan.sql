cd81967a056f86671da2947e3bdefe6645bd08df
theacebutler <theacebutler@gmail.com>
2026-06-07 15:49:23 -0400

    ADD a key field to usersTable

---
+++ api/drizzle/0012_hot_crusher_hogan.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;--> statement-breakpoint
+CREATE TABLE `__new_users_table` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`key` text DEFAULT 'fac2695c-5381-4cd5-b370-2ec0d4c002d4' NOT NULL,
+	`username` text NOT NULL,
+	`password` text NOT NULL,
+	`createdAt` text
+);
+--> statement-breakpoint
+INSERT INTO `__new_users_table`("id", "key", "username", "password", "createdAt") SELECT "id", "key", "username", "password", "createdAt" FROM `users_table`;--> statement-breakpoint
+DROP TABLE `users_table`;--> statement-breakpoint
+ALTER TABLE `__new_users_table` RENAME TO `users_table`;--> statement-breakpoint
+PRAGMA foreign_keys=ON;--> statement-breakpoint
+CREATE UNIQUE INDEX `users_table_username_unique` ON `users_table` (`username`);
