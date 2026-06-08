0eeca6831e8ee8929a855b57855801e19d5794cf
Avi Butler <Theacebutler@gmail.com>
2026-06-07 23:46:30 -0400

    Update inDevAlert.tsx

--- frontend/src/components/inDevAlert.tsx
+++ frontend/src/components/inDevAlert.tsx
@@ -3,7 +3,7 @@ export default function InDevAlert() {
     <div className="bg-indigo-600/10 border border-indigo-600/20 rounded-lg px-3 py-6 text-sm mx-3 mt-3 text-indigo-600 text-center">
       New: You can add resources via apple shortcuts. Check out the  <a
         className="underline hover:text-text"
-        href="https://github.com/theacebutler/linkIng"
+        href="https://github.com/theacebutler/linking#apple-screenshots-integration"
         target="_blank"
         rel="noreferrer"
       >
