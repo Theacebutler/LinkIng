cd24e2a3f4a2dbf31ad565bd8d45d23f4552b750
theacebutler <theacebutler@gmail.com>
2026-06-10 21:44:45 -0400

    ADD check for high cpu

--- api/utils/formatMemoryUsage.ts
+++ api/utils/formatMemoryUsage.ts
@@ -1,3 +1,4 @@
+import os from 'os';
 export default function formatMemoryUsage() {
   const data = process.memoryUsage();
   return {
@@ -7,3 +8,9 @@ export default function formatMemoryUsage() {
     external: `${(data.external / 1024 / 1024).toFixed(2)} MB`,
   };
 };
+
+export function highCPUload(): boolean {
+  const [avg1, _avg5, _avg15] = os.loadavg()
+  if (avg1! > 0.3) return true
+  return false
+}
