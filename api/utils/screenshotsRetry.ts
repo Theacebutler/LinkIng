c8168f4f0e1cffff1e07fb9fee431dfc132e2ffe
theacebutler <theacebutler@gmail.com>
2026-06-10 21:07:28 -0400

    ADD function to get failed screenshots from the db

---
+++ api/utils/screenshotsRetry.ts
@@ -0,0 +1,42 @@
+import { screenshotStack, type ScreenshotJob } from "./screenshot";
+import { db } from "../db";
+import { resourcesTable, screenshotsTable } from "../db/schema";
+import { eq } from "drizzle-orm";
+
+interface FailedShap {
+  screenshots_table: {
+    id: number;
+    resourceId: number;
+    hasImage: number | null;
+  };
+  resources_table: {
+    id: number;
+    resourceUrl: string | null;
+    createdAt: string | null;
+    updatedAt: string | null;
+  } | null;
+}
+
+export default async function handleFaildScreenshots() {
+  // we have a screenshotsTable with a screenshotsTable.resourceId that points
+  // to the resourceTable id, we need to get the screenshots without an image,
+  // then find the resource linked to them, and get the url from the resource.
+
+  const [failed]: FailedShap[] | undefined = await db.select()
+    .from(screenshotsTable)
+    .leftJoin(resourcesTable, eq(resourcesTable.id, screenshotsTable.resourceId))
+    .where(eq(screenshotsTable.hasImage, 0))
+    .limit(1)
+    .execute()
+  if (!failed || !failed?.resources_table || failed?.screenshots_table) return
+  const url = failed.resources_table?.resourceUrl
+  if (!url) return
+  const newJob: ScreenshotJob = {
+    url,
+    resourceId: failed.screenshots_table.resourceId,
+    isDone: false,
+    timeAdded: Date.now(),
+    timesTried: 0
+  }
+  screenshotStack.push(newJob)
+}
