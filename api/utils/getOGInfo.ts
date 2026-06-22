4cd490c2979695d1664c17531ff968e17391b1a0
theacebutler <theacebutler@gmail.com>
2026-06-22 19:32:04 -0400

    UPDATE get the image size from og

--- api/utils/getOGInfo.ts
+++ api/utils/getOGInfo.ts
@@ -3,6 +3,8 @@ interface OGimage {
   imageData?: string | void
   title?: string
   description?: string
+  width?: number,
+  height?: number,
 }
 
 export default async function getOGinfo(resourceUrl: string): Promise<OGimage> {
@@ -10,6 +12,8 @@ export default async function getOGinfo(resourceUrl: string): Promise<OGimage> {
   const doc = cheerio.load(res)
   const imageUrl = doc('meta[property="og:image"]').attr('content') || doc('meta[name="twitter:image"]').attr('content')
   const title = doc('meta[property="og:title"]').attr('content') || doc('meta[name="twitter:title"]').attr('content')
+  const height = doc('meta[property="og:image:height"]').attr('content') || doc('meta[name="twitter:image:height"]').attr('content')
+  const width = doc('meta[property="og:image:width"]').attr('content') || doc('meta[name="twitter:image:width"]').attr('content')
   // TODO: description is not being used yet
   const description = doc('meta[property="og:description"]').attr('content') || doc('meta[name="twitter:description"]').attr('content')
   if (imageUrl) {
@@ -22,6 +26,8 @@ export default async function getOGinfo(resourceUrl: string): Promise<OGimage> {
       .catch(console.error)
     return {
       imageData,
+      height: Number(height) || 1820,
+      width: Number(width) || 720,
       title,
       description
     }
