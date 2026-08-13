9e68c596c8e9946f357188a0b9fe74cfa01c91c9
Avi Butler <Theacebutler@gmail.com>
2026-08-13 14:42:37 -0400

    FIX get db connection from config

--- api/db/index.ts
+++ api/db/index.ts
@@ -1,8 +1,9 @@
 import { Database } from "bun:sqlite";
 import { drizzle } from "drizzle-orm/bun-sqlite";
 import { migrate } from "drizzle-orm/bun-sqlite/migrator";
+import { config } from "../config";
 
-const sqlite = new Database(process.env.DB_CONN as string);
+const sqlite = new Database(config.DB_CONN);
 export const db = drizzle({ client: sqlite })
 
 // run the migration when the server starts
