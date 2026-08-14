d6f7931fe6a3570f3fa404037dea865fc368224e
Avi Butler <Theacebutler@gmail.com>
2026-08-14 15:54:43 -0400

    ADD cache image function

--- frontend/src/utils/cache.ts
+++ frontend/src/utils/cache.ts
@@ -1,3 +1,7 @@
+/* take in an image as a base64 encoded string and save it to local storage */
+export async function cacheImage(url: string, data: string) {
+  localStorage.setItem(url, data)
+}
 
 export function getImage(url: string): string | null {
   const image = localStorage.getItem(url)
